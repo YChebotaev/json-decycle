a20c8114e671d1bd81aefc2b5611a486e2ce681b
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-05-14 02:21:54 +0200

    add another test case for escaped $ref

--- test/index.js
+++ test/index.js
@@ -154,3 +154,30 @@ const { decycle, retrocycle, extend } = require('../index')
   assert(result.test.$ref === '#/invalid')
 }
 
+
+{
+  const cycled = {
+    foo: {},
+    bar: {},
+    test: { $ref: "/invalid" }
+  }
+  
+  cycled.foo.bar = cycled.bar
+  cycled.bar.foo = cycled.foo
+  
+  assert(cycled.foo.bar === cycled.bar)
+  assert(cycled.bar.foo === cycled.foo)
+
+  
+  let result = JSON.stringify(cycled, decycle())
+  
+  assert.equal(result, '{"foo":{"bar":{"foo":{"$ref":"#/foo"}}},"bar":{"$ref":"#/foo/bar"},"test":{"$ref":"/invalid"}}')
+  
+  result = JSON.parse(result, retrocycle())
+  
+  assert(result.foo.bar === result.bar)
+  assert(result.bar.foo === result.foo)
+  assert(result.test !== undefined)
+  assert(result.test.$ref === '/invalid')
+}
+
